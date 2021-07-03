44c2c239890e7c604ceee4e5dbaaa4ac07c29c41
nooed <73084304+nooek@users.noreply.github.com>
2021-07-03 15:59:19 -0300

    navbar finished

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./styles.css";
+import Logo from "../../assets/images/logo.svg"
 
 const Navbar = () => {
-  return <div className="navbar">dsa</div>;
+  return (
+    <div className="navbar">
+      <div className="logo-text-container">
+        <img src={Logo} alt="Logo" className="logo" />
+        <h2 className="text">YOG</h2>
+      </div>
+      <div className="func-container">
+        <button className="login-btn">Login</button>
+        <button className="register-btn">Register</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
